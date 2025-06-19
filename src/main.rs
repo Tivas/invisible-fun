c50b399d4627ed714a3dd2fab93caa1ef4fb573d
Tivas <tivasl@gmail.com>
2025-06-19 10:16:21 +0200

    remove web_render

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-mod webpage_renderer;
 fn main() {
-    // webpage_renderer::render_page("./src/test.htm");
-    webpage_renderer::screenshot_thepage().expect("Failed to take screenshot");
     println!("Hello, world!");
 }
